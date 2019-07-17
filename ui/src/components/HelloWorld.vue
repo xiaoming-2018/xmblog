66485b4acc284edc4c9dda41018b17d22d9b478d
chenming <15810507892@163.com>
2019-07-17 08:46:18 +0800

    update home page

--- ui/src/components/HelloWorld.vue
+++ ui/src/components/HelloWorld.vue
@@ -1,8 +1,47 @@
 <template>
   <div class="hello">
-    <mavon-editor v-model="msg" :toolbars="toolbars" :editable="edit" @save="sss"/>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      :center="true"
+      active-text-color="#ffd04b">
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">技术杂谈</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">文章转载</template>
+        <el-menu-item index="3-1">选项1</el-menu-item>
+        <el-menu-item index="3-2">选项2</el-menu-item>
+        <el-menu-item index="3-3">选项3</el-menu-item>
+        <el-submenu index="3-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="3-4-1">选项1</el-menu-item>
+          <el-menu-item index="3-4-2">选项2</el-menu-item>
+          <el-menu-item index="3-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="4">个人归档</el-menu-item>
+      <el-menu-item index="5">我的项目</el-menu-item>
+      <el-menu-item index="6">给我留言</el-menu-item>
+      <el-menu-item index="7">关于我</el-menu-item>
+    </el-menu>
+    <!--<mavon-editor v-model="msg" :toolbars="toolbars" :editable="edit" @save="sss"/>-->
     <!--<mavon-editor v-model="msg" :toolbarsFlag="toolbarsFlag" :subfield="subfield" :defaultOpen="defaultOpen"/>-->
-    <button @click="save">保存</button>
+    <!--<button @click="save">保存</button>-->
   </div>
 </template>
 
@@ -50,7 +89,8 @@ export default {
       edit: true,
       toolbarsFlag: false,
       subfield: false,
-      defaultOpen: "preview"
+      defaultOpen: "preview",
+      activeIndex: '1'
     }
   },
   methods:{
@@ -62,24 +102,16 @@ export default {
       console.log("this is render"+render);
       console.log("this is value"+value);
     },
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.el-menu-demo{
+  margin-top: 200px;
 }
 </style>
--- ui/src/main.js
+++ ui/src/main.js
@@ -9,7 +9,10 @@ import mavonEditor from 'mavon-editor'
 //                 or
 //                 mavonEditor.markdownIt
 import 'mavon-editor/dist/css/index.css'
-Vue.use(mavonEditor)
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(mavonEditor);
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
